1443af385131751cccc2f3cbf6627159bf5ff1f5
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-07 10:12:07 -0300

    feat: add Deposit method

---
+++ cSharpBank/CheckingAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpBank
+{
+    public class CheckingAccount
+    {
+        public int agency;
+        public string account;
+        public string accountHolder;
+        public double balance;
+
+        public void Deposit(double amount)
+        {
+            this.balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (amount <= this.balance)
+            {
+                this.balance -= amount;
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public bool Transfer(double amount, CheckingAccount targetAccount)
+        {
+            if (this.balance < amount)
+            {
+                return false;
+            }
+            else
+            {
+                this.balance -= amount;
+                targetAccount.balance += amount;
+                return true;
+            }
+        }
+    }
+}
+ 
---
+++ cSharpBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using cSharpBank;
+
+CheckingAccount andrewAccount = new CheckingAccount();
+andrewAccount.accountHolder = "Andrew Stuart";
+andrewAccount.agency = 15;
+andrewAccount.account = "1010-6";
+andrewAccount.balance = 100;
+
+
+Console.WriteLine("Andrew balance: " + andrewAccount.balance);
+
+andrewAccount.Deposit(100);
+
+
+
+
+
+
