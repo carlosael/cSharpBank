1ce65db1decbf593161b0b66df425a308a8934c6
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-02-07 10:44:37 -0300

    feat: add Client class

--- cSharpBank/CheckingAccount.cs
+++ cSharpBank/CheckingAccount.cs
@@ -10,12 +10,37 @@ namespace cSharpBank
     {
         public int agency;
         public string account;
+        public double balance = 0;
         public string accountHolder;
-        public double balance;
 
         public void Deposit(double amount)
         {
             this.balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (amount <= this.balance)
+            {
+                this.balance -= amount;
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public bool Transfer(double amount, CheckingAccount targetAccount)
+        {
+            if (this.balance < amount)
+            {
+                return false;
+            }
+            else
+            {
+                this.Withdraw(amount);
+                targetAccount.Deposit(amount);
+                return true;
+            }
         }
     }
 }
--- cSharpBank/Program.cs
+++ cSharpBank/Program.cs
@@ -10,7 +10,21 @@ andrewAccount.balance = 100;
 Console.WriteLine("Andrew balance: " + andrewAccount.balance);
 
 andrewAccount.Deposit(100);
-andrewAccount.Withdraw(50);
+
+CheckingAccount maryAccount = new CheckingAccount();
+maryAccount.accountHolder = "Mary Lenon";
+maryAccount.agency = 17;
+maryAccount.account = "1010-8";
+maryAccount.balance = 350;
+
+Console.WriteLine("Mary balance: " + maryAccount.balance);
+
+andrewAccount.Transfer(50, maryAccount);
+
+Console.WriteLine("Andrew balance: " + andrewAccount.balance);
+Console.WriteLine("Mary balance: " + maryAccount.balance);
+
+
 
 
 
