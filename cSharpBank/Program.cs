8f5d2d544c31b2fbff05091317a5151eb08fc5f6
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-03-01 15:47:17 -0300

    fix: limit file stream to read only the right amount of bytes

--- cSharpBank/Program.cs
+++ cSharpBank/Program.cs
@@ -8,27 +8,32 @@ class Program
     static void Main(string[] args)
     {
         var fileAdress = "accounts.txt";
-        var bytesRead = -1;
 
-        var fileStream = new FileStream(fileAdress, FileMode.Open);
-        var buffer = new byte[1024];
-        bytesRead = fileStream.Read(buffer, 0, 1024);
-
-        while(bytesRead != 0) 
+        using(var fileStream = new FileStream(fileAdress, FileMode.Open))
         {
+            var bytesRead = -1;
+
+
+            var buffer = new byte[1024];
             bytesRead = fileStream.Read(buffer, 0, 1024);
-            WriteBuffer(buffer);
+
+            while (bytesRead != 0)
+            {
+                bytesRead = fileStream.Read(buffer, 0, 1024);
+                WriteBuffer(buffer, bytesRead);
+            }
+
+            fileStream.Close();
+            Console.ReadLine();
         }
         
-        fileStream.Close();
-        Console.ReadLine();
     }
 
-    static void WriteBuffer(byte[] buffer)
+    static void WriteBuffer(byte[] buffer, int bytesRead)
     {
         var utf8 = new UTF8Encoding();
 
-        var text = utf8.GetString(buffer);
+        var text = utf8.GetString(buffer, 0, bytesRead);
         Console.Write(text);
 
         //foreach (var myByte in buffer)
