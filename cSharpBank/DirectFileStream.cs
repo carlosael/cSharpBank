c8324f1218e03d0db16a342ed5440590cecfd915
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-03-01 15:54:10 -0300

    chore: move DirectFileStream to a folder

---
+++ cSharpBank/DirectFileStream.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void DirectFileStream(string[] args)
+    {
+        var fileAdress = "accounts.txt";
+
+        using (var fileStream = new FileStream(fileAdress, FileMode.Open))
+        {
+            var bytesRead = -1;
+
+
+            var buffer = new byte[1024];
+            bytesRead = fileStream.Read(buffer, 0, 1024);
+
+            while (bytesRead != 0)
+            {
+                bytesRead = fileStream.Read(buffer, 0, 1024);
+                WriteBuffer(buffer, bytesRead);
+            }
+
+            fileStream.Close();
+            Console.ReadLine();
+        }
+
+    }
+
+    static void WriteBuffer(byte[] buffer, int bytesRead)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var text = utf8.GetString(buffer, 0, bytesRead);
+        Console.Write(text);
+
+        //foreach (var myByte in buffer)
+        //{
+        //    Console.WriteLine(myByte);
+        //    Console.WriteLine(" ");
+        //}
+    }
+}
